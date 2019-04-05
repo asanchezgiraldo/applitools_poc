47f2955b24a9441cc3c2da93fa96d1b8f76bfb36
alejandro <alejandro.sanchez@qantasloyalty.com>
2019-04-05 22:33:56 +1100

    few commits

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -1,18 +1,19 @@
 describe("Qantas Insurance Website", () => {
-        it("Health", () => {
-            cy.visit("health-insurance/review");
-            cy.eyesOpen({
-                appName: 'Insurance World!',
-                testName: 'My first Insurance test!',
-                browser: [
-                    {width: 800, height: 600, name: 'firefox'},
-                    {width: 1024, height: 768, name: 'chrome'}
-                    //Add more variations
-                ],
-            });
-            cy.eyesCheckWindow('Online Health');
-            cy.eyesClose();
+
+    it("Health", () => {
+        cy.visit("health-insurance/review");
+        cy.eyesOpen({
+            appName: 'Insurance World!',
+            testName: 'My first Insurance test!',
+            browser: [
+                {width: 800, height: 600, name: 'firefox'},
+                {width: 1024, height: 768, name: 'chrome'}
+                //Add more variations
+            ],
         });
+        cy.eyesCheckWindow('Online Health');
+        process.env.APPTL = cy.eyesClose(true);
+    });
 });
 
 
